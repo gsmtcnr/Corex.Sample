64f020276665ad853308067b271fc9d9736d62ab
Muhammet Balamir <muhammet.balamir@tcm.com.tr>
2021-02-28 21:23:04 +0300

    UserRegister Input Validation Added

---
+++ Corex.Sample.Validation.InputValidation/Users/User/Input/UserRegisterInputValidation.cs
@@ -0,0 +1,11 @@
+﻿using Corex.Sample.Model.ViewModel.Users.User.Inputs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Corex.Sample.Validation.InputValidation.Users.User.Input
+{
+    public class UserRegisterInputValidation : ValidationBase<UserRegisterInputModel>
+    {
+    }
+}
