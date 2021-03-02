3b3524dc9cc677a62e4f492fdd3b298ef28d7f8a
sametcinar <sametcinar@msn.com>
2021-03-02 22:49:45 +0300

    program

--- Corex.Sample.Presentation.ConsoleApp/Program.cs
+++ Corex.Sample.Presentation.ConsoleApp/Program.cs
@@ -1,19 +1,14 @@
 ﻿using Corex.Sample.Core.Infrastructure;
-using Corex.Sample.Data.Infrastructure.Orders.Order;
-using Microsoft.Extensions.DependencyInjection;
 using System;
 
 namespace Corex.Sample.Presentation.ConsoleApp
 {
     class Program
     {
-        private static ServiceProvider _serviceProvider;
         static void Main(string[] args)
         {
 
             new CorexStartup();
-            var orderRepository = IoCManager.Resolve<IOrderRepository>();
-            var get = orderRepository.Get(s => s.Position==1);
             Console.WriteLine("Hello World!");
         }
 
